1f9692fd883b42ffecd29a0fcf946804bcf91808
Abdulaleam <167490133+Abdulaleam@users.noreply.github.com>
2026-07-12 01:28:02 +0300

    adding the command that i want to start the minigame

---
+++ src/main/java/rainy/randomcraft/ehStartCommand.java
@@ -0,0 +1,23 @@
+package rainy.randomcraft;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
+public class ehStartCommand {
+
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+        dispatcher.register(
+                CommandManager.literal("start")
+                        .executes(context -> {
+
+                            ChoasRandomizer.randomize();
+                            context.getSource().getServer().getPlayerManager().broadcast( Text.literal("Chaos Crafting Starts! HEHEHE!")
+                                            .formatted(Formatting.RED), false);
+                            return Command.SINGLE_SUCCESS;
+                        }));
+    }
+}
