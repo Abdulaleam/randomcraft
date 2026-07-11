7e08c72f3abdf913c18bdebeb6979af2eeb90ea3
Abdulaleam <167490133+Abdulaleam@users.noreply.github.com>
2026-07-12 01:06:36 +0300

    first commit , just making stuff ready for me to start

---
+++ src/main/java/rainy/randomcraft/RandomCraftClient.java
@@ -0,0 +1,10 @@
+package rainy.randomcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RandomCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/rainy/randomcraft/Randomcraft.java
@@ -0,0 +1,21 @@
+package rainy.randomcraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.util.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Randomcraft implements ModInitializer {
+	public static final String MOD_ID = "randomcraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	@Override
+	public void onInitialize() {
+
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.of(MOD_ID, path);
+	}
+}
