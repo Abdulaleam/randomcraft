23b1044bfab0bb5b45e90641d1af1e3d2464310e
Abdulaleam <167490133+Abdulaleam@users.noreply.github.com>
2026-07-13 13:26:10 +0300

    added mixin

---
+++ src/main/java/rainy/randomcraft/mixin/ShapedRecipeAccessor.java
@@ -0,0 +1,12 @@
+package rainy.randomcraft.mixin;
+
+import net.minecraft.recipe.RawShapedRecipe;
+import net.minecraft.recipe.ShapedRecipe;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(ShapedRecipe.class)
+public interface ShapedRecipeAccessor {
+    @Accessor("raw")
+    RawShapedRecipe getRaw();
+}
