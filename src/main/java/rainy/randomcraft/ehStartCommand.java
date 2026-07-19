c3758148378f9b5be303ca4246c9285b20439779
Abdulaleam <167490133+Abdulaleam@users.noreply.github.com>
2026-07-19 09:52:05 +0300

    added the command

--- src/main/java/rainy/randomcraft/ehStartCommand.java
+++ src/main/java/rainy/randomcraft/ehStartCommand.java
@@ -2,7 +2,6 @@ package rainy.randomcraft;
 
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.BoolArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
@@ -12,25 +11,28 @@ public class ehStartCommand {
 
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(
-                CommandManager.literal("start")
+                CommandManager.literal("chaosstart")
+                        .requires(source -> source.hasPermissionLevel(2))
                         .executes(context -> {
                             ChoasRandomizer.enabled = true;
+                            Randomcraft.ENABLED = true;
 
-                            context.getSource().getServer().getPlayerManager().broadcast( Text.literal("Chaos Crafting Starts! HEHEHE!")
-                                            .formatted(Formatting.RED), false);
+                            context.getSource().getServer().getPlayerManager().broadcast(
+                                    Text.literal("Chaos Crafting Starts! HEHEHE!").formatted(Formatting.RED), false);
                             return Command.SINGLE_SUCCESS;
                         }));
 
         dispatcher.register(
-                CommandManager.literal("stop")
+                CommandManager.literal("chaosstop")
+                        .requires(source -> source.hasPermissionLevel(2))
                         .executes(context -> {
                             ChoasRandomizer.enabled = false;
+                            Randomcraft.ENABLED = false;
                             ChoasRandomizer.restoreRecipes(context.getSource().getServer());
-                            context.getSource().getServer().getPlayerManager().broadcast( Text.literal("Awhhh We Gotta stop :( ")
-                                    .formatted(Formatting.AQUA), false);
+
+                            context.getSource().getServer().getPlayerManager().broadcast(
+                                    Text.literal("Awhhh We Gotta stop :(").formatted(Formatting.AQUA), false);
                             return Command.SINGLE_SUCCESS;
-
                         }));
-
     }
 }
